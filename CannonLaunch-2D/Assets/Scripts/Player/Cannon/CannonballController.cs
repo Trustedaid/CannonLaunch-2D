5ad2354021f0d004b5344653bc12516c70fad935
Trustedaid <92570103+Trustedaid@users.noreply.github.com>
2022-10-05 19:09:18 +0300

    Cannon,cannonball,enemy and basic cannonball movement impelemented
    Main elemernts are implemented
    projectile movement

---
+++ CannonLaunch-2D/Assets/Scripts/Player/Cannon/CannonballController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonballController : MonoBehaviour
+{
+
+
+    [HideInInspector] private Rigidbody2D rb;
+    [HideInInspector] private CircleCollider2D col;
+
+    [HideInInspector] public Vector3 pos { get { return transform.position; } }
+
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<CircleCollider2D>();
+
+    }
+
+    private void Start()
+    {
+        
+    }
+    void Update()
+    {
+
+    }
+    public void Launch (Vector2 force)
+    {
+        rb.AddForce(force, ForceMode2D.Impulse);
+    }
+    public void ActivateRb()
+    {
+        rb.isKinematic = false;
+    }
+
+    public void DesactivateRb()
+    {
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = 0f;
+        rb.isKinematic = true;
+    }
+}
